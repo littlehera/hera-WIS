b35a796c0cc615bd625370e923d125fc7cf0f4c1
Herabelle Villanueva <>
2013-09-23 13:51:14 +0800

    First Version - Still incomplete. Just trying out git commit. :)

---
+++ src/wis/WIS.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wis;
+
+/**
+ *
+ * @author HAB VILLANUEVA
+ */
+public class WIS {
+
+    private static empLogin login = new empLogin(); 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       login.setVisible(true);
+    }
+}
